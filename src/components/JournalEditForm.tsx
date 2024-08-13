8c0d6893a6d791aa0f2d913d61e8e98238aadadf
Tim Whitney <tim.d.whitney@gmail.com>
2024-08-13 14:00:37 -0600

    feat(ink-9): adds "create journal entry" button to home screen

---
+++ src/components/JournalEditForm.tsx
@@ -0,0 +1,41 @@
+import { Button, Group, TextInput } from "@mantine/core";
+import { Form } from "react-router-dom";
+import type { IJournal } from "../helpers/journals.ts";
+
+type Props = {
+	// It would be null when this is used to create a new journal.
+	journal: IJournal | null;
+	onSubmit: () => void;
+	onDelete: () => void;
+};
+export function JournalEditForm({ journal, onSubmit, onDelete }: Props) {
+	return (
+		<Form
+			method={journal ? "patch" : "post"}
+			onSubmit={onSubmit}
+			action={journal ? `/journal/${journal.id}/edit` : "/"}
+		>
+			<TextInput
+				name={"name"}
+				placeholder={"Journal Name"}
+				defaultValue={journal ? journal.name : ""}
+				size={"lg"}
+			/>
+			<Group justify={"space-between"}>
+				<Button type={"submit"} value={journal ? "Update" : "Create"} mt={"md"}>
+					{journal ? "Update" : "Create"}
+				</Button>
+				{journal && (
+					<Button
+						variant={"transparent"}
+						value={"Delete"}
+						mt={"md"}
+						onClick={onDelete}
+					>
+						Delete
+					</Button>
+				)}
+			</Group>
+		</Form>
+	);
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,15 @@
-import { Anchor, Container, List } from "@mantine/core";
-import { IconBook } from "@tabler/icons-react";
+import { Anchor, Button, Container, Flex, List } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconBook, IconPlus } from "@tabler/icons-react";
 import { Link, useLoaderData } from "react-router-dom";
+import { JournalEditModals } from "../components/JournalEditModals.tsx";
 import type { RootLoaderData } from "./root.tsx";
 
 export default function Index() {
+	const [
+		openedJournalEdits,
+		{ open: openJournalEdits, close: closeJournalEdits },
+	] = useDisclosure(false);
 	const { journals } = useLoaderData() as RootLoaderData;
 	return (
 		<Container size="xs">
@@ -16,14 +22,35 @@ export default function Index() {
 							</Anchor>
 						</List.Item>
 					))}
+					<List.Item>
+						<Anchor onClick={openJournalEdits}>
+							<Flex gap={"5px"}>
+								<IconPlus /> Create New Journal
+							</Flex>
+						</Anchor>
+					</List.Item>
 				</List>
 			) : (
 				<>
 					<h1>Ink.</h1>
 					<h2>A journal meant for journaling.</h2>
 					<p>This ain't your notes app, darlin`.</p>
+					<Button
+						leftSection={<IconPlus />}
+						fullWidth
+						onClick={openJournalEdits}
+						my={30}
+					>
+						Create New Journal
+					</Button>
 				</>
 			)}
+			<JournalEditModals
+				opened={openedJournalEdits}
+				close={closeJournalEdits}
+				journal={null}
+				setJournal={() => {}}
+			/>
 		</Container>
 	);
 }
